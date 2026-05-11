a332e414761652afcee3e0cf62dea7fd6e35b351
Ngirfanul Mangali <metalqwerty0@gmail.com>
2026-05-11 20:32:24 +0700

    setting docker

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-6">
+      <h2 className="text-2xl font-bold text-gray-800">E-Commerce Ku</h2>
+      <p className="mt-2 text-gray-600">Selamat datang di toko online saya!</p>
+      <button className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition">
+        Lihat Produk
+      </button>
+    </div>
+  </div>
+  )
+}
+
+export default App
