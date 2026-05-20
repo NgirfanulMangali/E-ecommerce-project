be11cb9de0ec2bef074b1a9bc4b1f8c8351d573f
Ngirfanul Mangali <metalqwerty0@gmail.com>
2026-05-20 22:37:00 +0700

    "add product images"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,9 @@ function App() {
   
 
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-6">
-      <h2 className="text-2xl font-bold text-gray-800">E-Commerce Ku</h2>
-      <p className="mt-2 text-gray-600">Selamat datang di toko online saya!</p>
-      <button className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition">
-        Lihat Produk
-      </button>
+    <div className="h-[34px] bg-black flex items-center justify-center">
+      <p className='text-white font-satoshi text-center text-xs'>Sign up and get 20% off to your first order.  <a href="/signup" className='underline'>Sign Up Now</a></p>
     </div>
-  </div>
   )
 }
 
