8ee4ae52ee2e8375adf50db205ac3327400e3c2b
Ngirfanul Mangali <metalqwerty0@gmail.com>
2026-07-08 16:27:24 +0700

    feat(auth): add sign up form using shadcn/ui

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Signup from "../components/Signup";
 
 
 function Register() {
   return (
     <div>
       <Header />
+      <Signup />
       <Footer />
     </div>
   )
